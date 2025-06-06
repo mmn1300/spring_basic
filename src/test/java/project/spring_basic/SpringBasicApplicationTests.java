6092f89cb9e82a46b0650f56be6eaf4f963e5d2a
mmn1300 <alsry3904@gamil.com>
2025-06-06 15:30:14 +0900

    Refactor code to prevent build errors

--- src/test/java/project/spring_basic/SpringBasicApplicationTests.java
+++ src/test/java/project/spring_basic/SpringBasicApplicationTests.java
@@ -2,8 +2,10 @@ package project.spring_basic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBasicApplicationTests {
 
 	@Test
